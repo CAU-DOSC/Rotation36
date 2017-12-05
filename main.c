23e30a23cc1557b842f2ac6e92318ad408fba3a9
JihoLee <33647638+soongle2@users.noreply.github.com>
2017-12-05 20:36:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,41 @@
 int main() {
 	time_t start, end;    
 	int count = 0;
+	while (1) {
+		if (gendata() == 0)break;
+		else {
+			if (count == 0) {
+				printhead();
+				count++;
+			}
+			char *str1 = (char*)malloc(sizeof(char)*(n + 1));
+			strcpy(str1, str);
+			start = clock();
+			trivial(str1);
+			end = clock();
+			time1 = (double)(end - start) / CLOCKS_PER_SEC;
+			strcpy(str1, str);
+			start = clock();
+			juggle(str1);
+			end = clock();
+			time2 = (double)(end - start) / CLOCKS_PER_SEC;
+			strcpy(str1, str);
+			start = clock();
+			blockswap(str1, d, n);
+			end = clock();
+			time3 = (double)(end - start) / CLOCKS_PER_SEC;
+			strcpy(str1, str);
+			start = clock();
+			reversal(str1);
+			end = clock();
+			time4 = (double)(end - start) / CLOCKS_PER_SEC;
+			printresult();
+			free(str1);
+		}
+	}
+	return 0;
+	/*time_t start, end;    
+	int count = 0;
         int n,d;
 	while (1) {
                 if (gendata(&d,&n) == 0)break;
@@ -22,5 +57,5 @@ int main() {
                         printresult(d,n);
 		}
 	}
-	return 0;
+	return 0;*/
 }
